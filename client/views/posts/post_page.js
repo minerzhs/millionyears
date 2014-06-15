f5e012014e5bb342e6ed8f74fcb4b213b56ad820
Haisu Zuo <zuohaisu@gmail.com>
2014-06-15 22:11:59 +0800

    page 207

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,4 +1,5 @@
 Template.postPage.helpers({ 
   comments: function() {
-    return Comments.find({postId: this._id}); }
+    return Comments.find({postId: this._id});
+  }
 });
--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,28 @@
 Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+  comment: function(commentAttributes){
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+
+    if(!commentAttributes.body)
+      throw new Meteor.Error(422, 'pls write sth');
+
+    if(!post)
+      throw new Meteor.Error(422, 'you must comment on a post');
+
+    comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+
+    comment._id = Comments.insert(comment);
+
+    createCommentNotification(comment);
+
+    return comment._id
+  }
+});
